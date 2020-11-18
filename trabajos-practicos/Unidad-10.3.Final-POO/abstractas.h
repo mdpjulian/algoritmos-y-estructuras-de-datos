c1efeec9083dc28c8ea30bf3013c55ebc5afe772
kunkkas <mdpjulian@gmail.com>
2020-11-18 11:56:22 -0300

    push tp_final

---
+++ trabajos-practicos/Unidad-10.3.Final-POO/abstractas.h
@@ -0,0 +1,33 @@
+#ifndef ABSTRACTAS_H
+#define ABSTRACTAS_H
+
+class Animal{
+	public:
+		virtual void eat();
+		virtual void sleep() =0;
+		void layANDnurse();
+		void SetName();
+		void GetName();
+		void SetCode(int c);
+		void GetAnimalType();
+
+
+	protected:
+		std::string name;
+		int code;
+};
+
+class Oviparous : public Animal{
+	public:
+		void layEggs();
+
+};
+
+class Mammal : public Animal{
+	public:
+		void nurse();
+
+};
+
+
+#endif // ABSTRACTAS_H
